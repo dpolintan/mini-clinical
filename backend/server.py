dffc7c26c70116151d9f84f169f7869bc414230d
Deanna Polintan <deanna_229@yahoo.com>
2025-10-21 13:16:56 -0400

    extract env and pin vpython versions

--- backend/server.py
+++ backend/server.py
@@ -2,13 +2,17 @@ from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 from schema import schema 
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:3000")],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
